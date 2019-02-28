7dd085b31a692bb23f62d315b9374c2dd85e0074
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-02-28 05:10:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  gets.board
-  gets.index
   if board[index] == "  "
-    return false
-    end
+    return FALSE
+  end
